6a7a95e3bad92bf0513810d025a66fa0e25d8d23
MrDoge225 <sonofmydad@live.com>
2019-08-13 14:57:19 +0200

    input configuration per layer

--- noded.js
+++ noded.js
@@ -21,14 +21,30 @@ app.get('/solution', function(req, res) {
         "username": "You!",
         "rooms": req.query.rooms,
         "layers": req.query.layers,
-        "time_mix": req.query.time_mix,
-        "time_paint": req.query.time_paint,
+        "time_mix": splitCommaSeparatedValues(req.query.time_mix),
+        "time_paint": splitCommaSeparatedValues(req.query.time_paint),
         "self_clean": req.query.self_clean,
-        "mixers": req.query.mixers
+        "mixers": req.query.mixers,
+        "jobStarted": new Date()
     };
-    res.render('index.html', formInputs);
+
+    const planFileContent = fs.readFileSync('plan.json')
+    const plan=JSON.parse(planFileContent);
+
+    res.render('index.html', { inputs: formInputs, solution: plan});
 });
 
 app.get('/d',(req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+/**
+ * Splits the text by comma, trims, parses and returns as a list of numbers.
+ * @param {string} valuesCsv comma separated
+ * @returns {mumber[]} list of values
+ */
+function splitCommaSeparatedValues(valuesCsv) {
+    return valuesCsv.split(',')
+        .map(valueAsString => valueAsString.trim()) // trim extra spaces
+        .map(valueAsString => Number.parseFloat(valueAsString)); // turn to number
+}
